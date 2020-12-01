4672128108343e1e345e214fb7f65e34a3295525
Rasyadi Abdoellah <rfabdoellah@gmail.com>
2020-12-01 20:56:46 +0700

    Add comments to data.js

--- data.js
+++ data.js
@@ -13,14 +13,41 @@ function coinFlip() {
 const types = ['event', 'article']
 const categories = ['finance', 'creative', 'tech']
 
+
 const postData = []
 const bannerData = []
 
+function genAuthor() {
+  return {
+    name: faker.name.findName(),
+    imgUrl: faker.image.people(128,128)
+  }
+}
+
+function genContent() {
+  return '<p>' + faker.lorem.paragraphs() + '</p>'
+}
+
 //generate array of dummy posts
 for(let i = 0 ; i < 1000; i++){
+  
+  const comment = {
+    id: 1,
+    author: genAuthor(),
+    content: genContent()
+  }
+  
+  //Mock comment data with an array of replies
+  const comments = {
+    ...comment,
+    replies: [
+      {content: genContent(), author: genAuthor(), id: 2, parent: {id: 1, author: comment.author}},
+      {content: genContent(), author: genAuthor(), id: 3, parent: {id: 1, author: comment.author}},
+    ]
+  }
 
   //generate dummy content and exerpt
-  const dummyContent = '<p>' + faker.lorem.paragraphs()
+  const dummyContent = '<p>' + faker.lorem.paragraphs() + '</p>'
   const excerpt =  dummyContent.slice(0, 200)
   const title = faker.lorem.sentence()
 
@@ -32,10 +59,7 @@ for(let i = 0 ; i < 1000; i++){
     excerpt: excerpt + ( dummyContent.length > 200 ? '...' : '') + (excerpt.endsWith('</p>') ? '' : '</p>'),
     content: dummyContent,
     category: [categories[i%3], categories[(Math.floor(Math.random() * 3))]], //#finance, #creative, #tech. For UI and filtering. Post can have 1 or more categories. First element in array cycles through categories, second element is randomly picked
-    author: {
-      name: faker.name.findName(),
-      imgUrl: faker.image.people(128,128)
-    },
+    author: genAuthor(),
     imgUrl: coinFlip() ? "https://jenius-cocreate.s3.ap-southeast-1.amazonaws.com/assets/2020/11/25134436/feature-bener.png" : '',
     datePublished : addDays(new Date('2019-01-01T09:00:00+07:00'), i),
     url : faker.internet.url(),
@@ -44,6 +68,7 @@ for(let i = 0 ; i < 1000; i++){
       title: `Jenius CoCreate - ${title}`,
     },
     pinned: coinFlip(),
+    comments:[{...comments}]
   }
   if(dummyPost.type === types[0]) {
     //IF POST TYPE = EVENT
